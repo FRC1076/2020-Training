7ccdeb3f2180fdc40beff82119b31f1f05d25283
Amy Liebowitz <amylieb@umich.edu>
2019-12-18 19:43:16 -0500

    Adding robotmap file

---
+++ robotmap.py
@@ -0,0 +1,16 @@
+deadzone = 0.2
+
+# omni = {
+#     'left_motor': 7,
+#     'right_motor': 7,
+#     'front_strafe': 7,
+#     'back_strafe': 3
+# }
+
+
+omni = {
+    'left_motor': 5,
+    'right_motor': 1,
+    'front_strafe': 2,
+    'back_strafe': 3
+}
